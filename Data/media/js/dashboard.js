72d88561953a949ab8c07936e2d80bc002f5e10d
sterlingbaldwin <baldwin32@llnl.gov>
2015-02-04 08:31:06 -0800

    getting the horizontal window spawning to work

--- Data/media/js/dashboard.js
+++ Data/media/js/dashboard.js
@@ -1,6 +1,9 @@
 $("body").ready(function(){
 
-
+	var panelArray = [];
+	var gridSize = 10;
+	var rowCount = 0;
+	var colCount = 0;
 
 	var config = {
 		id: function(){ return 'jsPanel_' + ( $('.jsPanel').length + 1 ) },
@@ -12,26 +15,20 @@ $("body").ready(function(){
 		},
 		selector: '#o-draggable',
 		size: {
-			width: $("#o-draggable").width()-3, 
-			height: $("#o-draggable").height()-33,
+			width: $("#o-draggable").width(), 
+			height: $("#o-draggable").height()-30,
 		}
     };
 
 
 
-
-	panelArray = [];
-
-
-
 	$("#spawn").click(function(e){
 		if(panelArray.length == 0) {
-			var mouseX = e.pageX;
-			var mouseY = e.pageY;
 			var panel = $.jsPanel(config);
 			panelArray.push(panel);
 			panel.find(".jsPanel-btn-close").click(function(){
 				panelArray.splice(panelArray.indexOf(panel), 1);
+				resizePanels();
 			});
 		} else {
 			var panel = $.jsPanel(config);
@@ -39,7 +36,21 @@ $("body").ready(function(){
 			panelArray.push(panel);
 			panel.find(".jsPanel-btn-close").click(function(){
 				panelArray.splice(panelArray.indexOf(panel), 1);
+				resizePanels();
 			});
+			resizePanels();
+			panel.show();
+		}
+	});
+
+	function snapToGrid() {
+		if (rowCount == 0 && colCount == 0) {
+
+		}
+	}
+
+	function resizePanels() {
+		if (panelArray.length > 0) {
 			var curPanel = $(panelArray[0]);	
 			$(curPanel).width($(curPanel).width() - ($(curPanel).width() - $(curPanel.parent()).width()/panelArray.length) );
 			var panelWidth = curPanel.width();
@@ -48,10 +59,9 @@ $("body").ready(function(){
 				curPanel.width(panelWidth);
 				var offset = $(panelArray[i-1]).offset();
 				curPanel.offset({left: offset.left+panelWidth});
-				curPanel.show();
 			}
 		}
-	});
+	}
 
 	
 
